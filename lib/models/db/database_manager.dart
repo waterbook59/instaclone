69f15c2c43fae6df95f148d2e764602fd3771ecd
waterbook59 <takeshi.m64@gmail.com>
2020-08-14 05:37:46 +0900

    googleログイン＆Firebase認証 認証ユーザーがDBにいるかどうか確認していなければ登録 Googleアカウントでログイン=>HomeScreen chapter.47-51

--- lib/models/db/database_manager.dart
+++ lib/models/db/database_manager.dart
@@ -3,23 +3,38 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instaclone/data_models/user.dart';
 
-class DatabaseManager{
+class DatabaseManager {
   //firestoreのインスタンスとってくる
   final Firestore _db = Firestore.instance;
 
 //cloudfirestoreの検索条件を指定して読み込む
-  Future<bool> searchUserInDb(FirebaseUser firebaseUser) async{
+  Future<bool> searchUserInDb(FirebaseUser firebaseUser) async {
     //usersというコレクションの中にあるuserIdがfirebaseUserのuidと同じ場合、ドキュメントを取ってくる
-    final query = await _db.collection("users")
-        .where("userId",isEqualTo: firebaseUser.uid)
+    final query = await _db
+        .collection("users")
+        .where("userId", isEqualTo: firebaseUser.uid)
         .getDocuments();
-    if(query.documents.length> 0){
+    if (query.documents.length > 0) {
       return true;
     }
     //documentがなければ
     return false;
   }
 
+  //登録なのでvoidで良い
+  Future<void> insertUser(User user) async {
+    //DartDataClassとtoMapのコンボでいい感じにCloud Firestoreに登録される
+    await _db.collection("users").document(user.userId).setData(user.toMap());
+  }
 
+  Future<User> getUserInfoFromDbById(String userId) async {
+    //検索条件を使ってデータ取ってくる、queryに格納してそれをmap化したものをfromMapでほぐす
+    final query = await _db
+        .collection("users")
+        .where("userId", isEqualTo: userId)
+        .getDocuments();
+    return User.fromMap(query.documents[0].data);
+  }
 }
--- lib/view/login/screens/login_screen.dart
+++ lib/view/login/screens/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:instaclone/generated/l10n.dart';
 import 'package:instaclone/style.dart';
 import 'package:instaclone/view/common/components/button_with_icon.dart';
+import 'package:instaclone/view/home_screen.dart';
 import 'package:instaclone/view_models/login_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -11,29 +13,50 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            //Billabongが日本語対応していないのでintl_ja.arb/appTitleは英語に戻す
-            Text(
-              S.of(context).appTitle,
-              style: loginTitleTextStyle,
-            ),
-            SizedBox(height: 8.0,),
-            ButtonWithIcon(
-              iconData: FontAwesomeIcons.signInAlt,
-              label:S.of(context).signIn,
-              onPressed: ()=>login(context),
-            ),
-          ],
+        child: Consumer<LoginViewModel>(
+          builder: (context, model, child) {
+            return model.isLoading
+                ? CircularProgressIndicator()
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      //Billabongが日本語対応していないのでintl_ja.arb/appTitleは英語に戻す
+                      Text(
+                        S.of(context).appTitle,
+                        style: loginTitleTextStyle,
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      ButtonWithIcon(
+                        iconData: FontAwesomeIcons.signInAlt,
+                        label: S.of(context).signIn,
+                        onPressed: () => login(context),
+                      ),
+                    ],
+                  );
+          },
         ),
       ),
     );
   }
 
-  login(BuildContext context) async{
-    final loginVieModel = Provider.of<LoginViewModel>(context,listen: false);
+  login(BuildContext context) async {
+    final loginVieModel = Provider.of<LoginViewModel>(context, listen: false);
     await loginVieModel.signIn();
-    //todo
+    if (!loginVieModel.isSuccessful) {
+      //ログイン失敗した場合toast
+      Fluttertoast.showToast(msg: S.of(context).signInFailed);
+      return;
+    }
+    //ログイン成功した場合
+    _openHomeScreen(context);
+  }
+
+  void _openHomeScreen(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_) => HomeScreen()),
+    );
   }
 }
